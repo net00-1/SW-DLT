02da1e7db6e0fda3db2f3f26d0b4965116285386
net00 <53622316+net00-1@users.noreply.github.com>
2020-11-04 23:37:33 -0600

    Update SWDLTPyGS.py

--- Scripts/SWDLTPyGS.py
+++ Scripts/SWDLTPyGS.py
@@ -1,8 +1,10 @@
 import subprocess
 
-print("Gathering URLs, please wait until you are redirected to Shortcuts. Process can take longer for bigger albums")
+print("Gathering URLs... Process will take longer for bigger albums.")
 
-subprocess.run("gallery-dl -g mediaURL  > SWDLTTempURLs.txt")
+mediaURL = 'mediaURL'
+
+subprocess.run("gallery-dl -g " + mediaURL + "GDLItems > SWDLTTempURLs.txt")
 
 subprocess.run("open shortcuts://run-shortcut?name=SW-DLT&input=text&text=%7B%22fName%22:%22gDownF%22%7D")
 subprocess.run("exit")
