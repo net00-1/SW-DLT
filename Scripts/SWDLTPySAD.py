f0d879ad20a5fd10d89c5caf9322471de172081e
net00 <53622316+net00-1@users.noreply.github.com>
2020-11-04 19:34:52 -0600

    Create SWDLTPySAD.py

---
+++ Scripts/SWDLTPySAD.py
@@ -0,0 +1,22 @@
+import youtube_dl
+import subprocess
+
+#SW-DLT Audio Download Script
+
+#Get format for the audio, or video if audio is unavailable
+
+userOpts = {"format":"genVars (defaultAD)","outtmpl":"SWDLTTempDL.%(ext)s","fixup":"never"}
+mediaURL = 'mediaURL'
+
+with youtube_dl.YoutubeDL(userOpts) as vidObj:
+	vidInfo = vidObj.extract_info(mediaURL, download=False)
+
+#Download audio, or video if audio is unavailable
+
+with youtube_dl.YoutubeDL(userOpts) as vidObj:
+	vidObj.download([mediaURL])
+	
+#Return to SWDLT with a specific function code
+		
+subprocess.run("open shortcuts://run-shortcut?name=SW-DLT&input=text&text=%7B%22fName%22:%22svaOutF%22,%22fType%22:%22Audio%22%7D")
+subprocess.run("exit")
