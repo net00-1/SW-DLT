c74318aebb7e2cf991ed0da6aaee2883c16182dc
net00 <53622316+net00-1@users.noreply.github.com>
2020-11-06 12:27:21 -0600

    Update SWDLTPyVD.py

--- Scripts/SWDLTPyVD.py
+++ Scripts/SWDLTPyVD.py
@@ -1,36 +1,15 @@
 import youtube_dl
 import subprocess
-import re
 
 #Get format for the video matching or closest to user requirements
 
-userOpts = {"format":"downOpts","outtmpl":"SWDLTTempDL.%(ext)s","fixup":"never"}
+userOpts = {"format":"downOpts","outtmpl":"SWDLTTempDL.%(ext)s","fixup":"detect_or_warn"}
 mediaURL = 'mediaURL'
-
-with youtube_dl.YoutubeDL(userOpts) as vidObj:
-	vidInfo = vidObj.extract_info(mediaURL, download=False)
-	outExt = vidInfo.get("ext",None)
-	outF = vidInfo.get("format",None)
 
 #Download video matching or closest to user requirements
 
 with youtube_dl.YoutubeDL(userOpts) as vidObj:
 	vidObj.download([mediaURL])
-
-#Invoke ffmpeg to fix up malformed MP4s, skip for other formats & skip for all merged videos
-
-if "+" not in outF:
-	
-	#Filters out merged with ffmpeg already
-	
-	if "mp4" in outExt:
-		#Filters out anything besides mp4
-		
-		print("PROCESSING MP4: Please wait until you are redirected back to Shortcuts.")
-		
-		subprocess.run("ffmpeg -i SWDLTTempDL." + outExt + " -c copy SWDLTFixedDL." + outExt)
-		subprocess.run("rm -f SWDLTTempDL." + outExt)
-		subprocess.run("mv SWDLTFixedDL." + outExt + " SWDLTTempDL." + outExt)
 
 #Return to SWDLT with a specific function code
 
