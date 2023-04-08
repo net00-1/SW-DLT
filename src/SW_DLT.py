4be85a802b81dc69c195336542dde93f056bb81e
net001 <netito2808@hotmail.com>
2023-04-08 16:30:42 -0600

    Added check for chardet lib

--- src/SW_DLT.py
+++ src/SW_DLT.py
@@ -75,19 +75,25 @@ class SW_DLT:
     @staticmethod
     def validate_install():
         reboot = False
-        show_progress("util", 0, 4)
+
+        show_progress("util", 0, 5)
+        if importlib.util.find_spec("chardet") is None:
+            subprocess.run("pip -q install chardet --disable-pip-version-check --upgrade")
+            reboot = True
+
+        show_progress("util", 1, 5)
         if importlib.util.find_spec("yt_dlp") is None:
             subprocess.run(
                 "pip -q install yt-dlp --disable-pip-version-check --upgrade --no-dependencies")
             reboot = True
 
-        show_progress("util", 1, 4)
+        show_progress("util", 2, 5)
         if importlib.util.find_spec("gallery_dl") is None:
             subprocess.run(
                 "pip -q install gallery-dl --disable-pip-version-check --upgrade")
             reboot = True
 
-        show_progress("util", 2, 4)
+        show_progress("util", 3, 5)
         if reboot:
             raise Exception(Consts.REBOOT_EXC)
 
@@ -105,13 +111,13 @@ class SW_DLT:
                 with open(f"{os.environ['HOME']}/Documents/bin/ffprobe.wasm", 'wb') as ffprobe:
                     ffprobe.write(req1.content)
 
-            show_progress("util", 3, 4)
+            show_progress("util", 4, 5)
             if not os.path.exists(f"{os.environ['HOME']}/Documents/bin/ffmpeg.wasm"):
                 req2 = requests.get(Consts.FFMPEG_URL)
                 with open(f"{os.environ['HOME']}/Documents/bin/ffmpeg.wasm", 'wb') as ffmpeg:
                     ffmpeg.write(req2.content)
 
-        show_progress("util", 4, 4)
+        show_progress("util", 5, 5)
 
     @staticmethod
     def erase_dependencies():
